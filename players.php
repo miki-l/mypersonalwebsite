440fd6392863d94931b53693547f5aecde9e3151
miki-l <ritremikilangbang@gmail.com>
2020-12-04 19:25:09 +0530

    add a delete button

--- players.php
+++ players.php
@@ -1,15 +1,25 @@
 <?php 
 
-  //connect to the database
-  $conn = mysqli_connect('localhost', 'Miki', 't8tc4AwT5CpOwqdo', 'sfg');
+include('config/db_connect.php');
 
-  //check connection
-  if(!$conn){
-    echo 'Connection error: ' . mysqli_connect_error();
+if(isset($_POST['delete'])){
+  $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+  $sql = "DELETE FROM players WHERE id = $id_to_delete";
+
+  if(mysqli_query($conn, $sql)){
+    // sucess
+    header('Location: index.php');
   }
+  else{
+    // fialure
+    echo 'query error: ' . mysql_eror($conn);
+  }
+}
+
+
 
   // write query from sfg
-  $sql = 'SELECT fname, lname, id FROM players ORDER BY create_at';
+  $sql = 'SELECT fname, lname, id, create_at FROM players ORDER BY create_at';
 
   //make query and get result
   $result = mysqli_query($conn, $sql);
@@ -21,6 +31,7 @@
 
   //close connection
   mysqli_close($conn);
+
 
   //explode(',', $players[0]['ingredient'])
 
@@ -41,10 +52,17 @@
         <div class="col s6 md3">
           <div class="card z-depth-0">
             <div class="card-content">
-              <div><?php echo htmlspecialchars($players['id']); ?> .
-                   <?php echo htmlspecialchars($players['fname']); ?> 
-                   <?php echo htmlspecialchars($players['lname']);?>  
+              <div>
+                <?php echo htmlspecialchars($players['id']); ?> 
+                <?php echo htmlspecialchars($players['fname']); ?> 
+                <?php echo htmlspecialchars($players['lname']);?> <br>
+                <?php echo htmlspecialchars($players['create_at']);?>  
               </div>
+
+              <form id="del" action="players.php" method="post">
+                <input type="hidden" name="id_to_delete" value="<?php echo $players['id'] ?>">
+                <input type="submit" name="delete" value="Delete" class="del_btn brand z-depth-0">
+              </form>
             </div>
             <!-- <div class="card-action right-align">
             <a href="#" class="branf-text">more info</a>
